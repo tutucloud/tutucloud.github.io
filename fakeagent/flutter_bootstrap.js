79eb4b315b186408808f35f61b6d646d40eb7f3c
Cloud Cao <tutu_cloud@yahoo.com>
2026-09-24 10:50:36 +0800

    deploy: build output

--- fakeagent/flutter_bootstrap.js
+++ fakeagent/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"a804b261645ef8c13eb3d5c44a5c2fb0340c55
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2585419142" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2122791869" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
